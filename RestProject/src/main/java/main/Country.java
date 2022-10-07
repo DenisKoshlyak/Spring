8df92c232a76141bfcbf88ce1f473551a93488b8
DenisKoshlyak <106622257+DenisKoshlyak@users.noreply.github.com>
2022-10-07 19:03:44 +0300

    RestControllerAndResponseEntity

---
+++ RestProject/src/main/java/main/Country.java
@@ -0,0 +1,29 @@
+package main;
+
+public class Country {
+	private String name;
+	private int population;
+	
+	public static Country of(String name, int population) {
+		Country c = new Country();
+		c.setName(name);
+		c.setPopulation(population);
+		return c;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+}
